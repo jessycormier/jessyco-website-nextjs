d6edd14da56b919b4091b1bb93f84cd11136f2bb
Jessy <jessycormier@gmail.com>
2021-09-06 22:25:22 -0300

    add layout component and update now page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,25 @@
-import Head from "next/head";
-import LayoutFooter from "../components/layout/footer";
-import LayoutHeader from "../components/layout/header";
+import Link from "next/link";
+import React from "react";
+import CenterContentLayout from "../components/layout/center-content.layout";
 
 export default function Index() {
   return (
-    <>
-      <Head>
-        <title>Jessy.Co</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.4/css/brands.css"
-          integrity="sha384-S5yUroXKhsCryF2hYGm7i8RQ/ThL96qmmWD+lF5AZTdOdsxChQktVW+cKP/s4eav"
-          crossOrigin="anonymous"
-        />
+    <CenterContentLayout>
+      <img
+        className="rounded-full border-8 border-gray-800 dark:border-gray-700 mt-16"
+        src="/profile.jpg"
+        width={200}
+        height={200}
+      />
 
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css"
-          integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <LayoutHeader></LayoutHeader>
+      <p className="mt-8">
+        Hey, thanks for visiting my website. My name is Jessy Cormier and I
+        build front-end stuff.
+      </p>
 
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center max-w-lg">
-          <img
-            className="rounded-full border-8 border-gray-800 dark:border-gray-700 mt-16"
-            src="/profile.jpg"
-            width={200}
-            height={200}
-          />
-
-          <p className="mt-8">
-            Hey, thanks for visiting my website. My name is Jessy Cormier and I
-            build front-end stuff.
-          </p>
-        </main>
-
-        <LayoutFooter></LayoutFooter>
-      </div>
-    </>
+      <p className="mt-4">
+        See what I'm up to <Link href="/now"><a>Now</a></Link>.
+      </p>
+    </CenterContentLayout>
   );
 }
--- pages/now.tsx
+++ pages/now.tsx
@@ -1,40 +1,27 @@
-import Head from "next/head";
-import LayoutFooter from "../components/layout/footer";
-import LayoutHeader from "../components/layout/header";
+import CenterContentLayout from "../components/layout/center-content.layout";
 
 export default function NowPage() {
   return (
-    <>
-      <Head>
-        <title>Jessy.Co - Now</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.4/css/brands.css"
-          integrity="sha384-S5yUroXKhsCryF2hYGm7i8RQ/ThL96qmmWD+lF5AZTdOdsxChQktVW+cKP/s4eav"
-          crossOrigin="anonymous"
-        />
+    <CenterContentLayout>
+      <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-blue-400 to-purple-800">
+        Now
+      </h1>
+      <ul className="mt-16">
+        <li>Developing this website while learning NextJS and Tailwind.</li>
+        <li>Self improvement like trying to have an growth mindset</li>
+        <li>Planning for building a house with my wife for our family</li>
+        <li>Playing a few games: Forger, Mass Effect, No Mans Sky</li>
+      </ul>
 
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css"
-          integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <LayoutHeader></LayoutHeader>
-
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center max-w-lg">
-          <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-blue-400 to-purple-800">Now</h1>
-          <ul className="mt-16">
-            <li>Developing this website</li>
-            <li>Self improvement</li>
-            <li>Planning for building a house</li>
-            <li>Playing a few games: Forger, Mass Effect, No Mans Sky</li>
-          </ul>
-        </main>
-      </div>
-    </>
+      <p className="mt-16">
+        I would like to drive this page from an api call instead of a static generated content. This way it could be driven with new details any time of the day.
+      </p>
+      <small className="mt-32">
+        What is a <strong>Now</strong> page? find out here{" "}
+        <a href="https://nownownow.com/about" target="_blank">
+          nownownow.com/about
+        </a>
+      </small>
+    </CenterContentLayout>
   );
 }
