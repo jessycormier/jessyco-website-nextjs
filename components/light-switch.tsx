fcd6c98bb6c3e056483037a4ff1e17441644c5a5
Jessy <jessycormier@gmail.com>
2022-08-19 16:53:18 -0300

    add next-theme to enable smooth darkmode on load

--- components/light-switch.tsx
+++ components/light-switch.tsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react";
+import { useTheme } from 'next-themes';
 import moonIcon from "../app/moon-icon";
 import sunIcon from "../app/sun-icon";
 
-
 export default function LightSwitch() {
-  const [currentTheme, setTheme] = useState(
-    typeof window !== "undefined" && localStorage.theme as string || "dark"
-  );
+  // const [currentTheme, setTheme] = useState(
+  //   typeof window !== "undefined" && localStorage.theme as string || "dark"
+  // );
   
-  function toggleTheme() {
-    console.log("toggleTheme");
-    setTheme(theme => theme === "dark" ? "light" : "dark");
-  }
+  // function toggleTheme() {
+  //   console.log("toggleTheme");
+  //   setTheme(theme => theme === "dark" ? "light" : "dark");
+  // }
 
-  useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove("light", "dark");
-    root.classList.add(currentTheme);
+  // useEffect(() => {
+  //   const root = window.document.documentElement;
+  //   root.classList.remove("light", "dark");
+  //   root.classList.add(currentTheme);
 
-    if (typeof window !== "undefined") {
-      localStorage.setItem("theme", currentTheme);
-    }
+  //   if (typeof window !== "undefined") {
+  //     localStorage.setItem("theme", currentTheme);
+  //   }
 
-  }, [currentTheme]);
+  // }, [currentTheme]);
 
-
+  const { theme, setTheme } = useTheme();
+      
   return (
     <button
       className="absolute top-4 right-4 cursor-pointer w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-700"
-      onClick={toggleTheme}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
-      {currentTheme === "light" ? sunIcon() : moonIcon()}
+      {theme === "light" ? sunIcon() : moonIcon()}
     </button>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,16 @@
-/* eslint-disable @next/next/no-before-interactive-script-outside-document */
+import { ThemeProvider } from "next-themes";
 import Head from "next/head";
-import Script from "next/script";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Head>
         <title>Jessy.Co</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
       </Head>
-      <Script
-        src="/scripts/init-theme.function.js"
-        strategy="beforeInteractive"
-      />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
